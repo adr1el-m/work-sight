5bb2ee60fd907ae46842edbae7048dfec418441b
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-07-23 01:13:57 +0800

    chore: reorganize files for improved project structure (#1)
    - Moved and updated file imports to reflect new organization
    - Ensured styles and assets are referenced from their correct locations
    - Updated documentation/comments where necessary

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/assets/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -15,34 +15,34 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+        Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        className={styles.primary}
+        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+        <Image
+          className={styles.logo}
+          src="/assets/vercel.svg"
+          alt="Vercel logomark"
+          width={20}
+          height={20}
+        />
+        Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
+        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.secondary}
           >
-            Read our docs
+        Read our docs
           </a>
         </div>
       </main>
